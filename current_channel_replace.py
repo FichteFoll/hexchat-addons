b216cf09721b55f30a51b53a0f4516928cf4f655
FichteFoll <fichtefoll2@googlemail.com>
2015-06-19 23:31:23 +0200

    Do not substitute when shift is pressed

--- current_channel_replace.py
+++ current_channel_replace.py
@@ -2,11 +2,15 @@ import hexchat
 
 
 __module_name__ = 'Current Channel Replace'
-__module_version__ = '0.1'
+__module_version__ = '0.2'
 __module_description__ = 'Replaces "#" with current channel'
 
 KEY_TAB = 65289
 KEY_SPACE = 32
+
+KEY_MOD_SHIFT = 1
+KEY_MOD_CTRL = 4
+KEY_MOD_ALT = 8
 
 
 def debug(*args, **kwargs):
@@ -17,14 +21,21 @@ def debug(*args, **kwargs):
 
 
 def on_key_press(word, word_eol, userdata):
-    """Perform substitution of # on tab key.
+    """Performs substitution of "#" on tab and space keys, unless shift is hold.
     """
     msg = hexchat.get_info('inputbox')
+    # https://github.com/hexchat/hexchat/issues/869
     key = int(word[0])
+    modifier = int(word[1])
+
     if msg is None:
         return
 
-    if msg.endswith("#") and key in (KEY_TAB, KEY_SPACE):
+    if (
+        msg.endswith("#")
+        and key in (KEY_TAB, KEY_SPACE)
+        and not (modifier & KEY_MOD_SHIFT)
+    ):
         msg = msg[:-1] + hexchat.get_info('channel')
         # debug("Replacing with", hexchat.get_info('channel'))
         hexchat.command("settext %s" % msg)
@@ -33,4 +44,4 @@ def on_key_press(word, word_eol, userdata):
 
 hexchat.hook_print('Key Press', on_key_press)
 
-print("\00304%s successfully loaded." % __module_name__)
+print("%s %s loaded" % (__module_name__, __module_version__))
