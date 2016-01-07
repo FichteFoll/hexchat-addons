565e57dfb6b280a6792adf75fdf29464de20ab92
FichteFoll <fichtefoll2@googlemail.com>
2016-01-07 18:37:40 +0100

    Change how prefixes are built in PluginPref

--- pluginpref/__init__.py
+++ pluginpref/__init__.py
@@ -10,12 +10,14 @@ from collections.abc import MutableMapping
 import hexchat
 
 
-__version__ = (0, 2, 0)
+__version__ = (0, 2, 0, '+')
 __author__ = "FichteFoll <fichtefoll2@googlemail.com>"
 
 
 __all__ = (
     "PluginPref",
+    "SerializablePluginPref",
+    "JSONPluginPref",
 )
 
 
@@ -24,6 +26,8 @@ class PluginPref(MutableMapping):
 
     All settings are internally prefixed with a string
     that is passed to the constructor.
+    The prefix will usually be built from the module name
+    but can be overridden.
 
 
     Limitations of pluginpref:
@@ -37,7 +41,7 @@ class PluginPref(MutableMapping):
     Example usage:
 
     >>> __module_name__ = "Your Awesome Addon Name"
-    >>> pref = PluginPref(__module_name__)  # or __module_name__.replace(" ", "_").lower()
+    >>> pref = PluginPref(__module_name__)
     >>> pref["a_setting"] = "a value"
     >>> "a_setting" in pref
     True
@@ -47,13 +51,18 @@ class PluginPref(MutableMapping):
     >>> pref["a_number"]
     123
     >>> pref["a_boolean"] = True
+
     >>> list(pref.items())
     [("a_setting": "a_value"), ('a_number': 123), ('a_boolean': 1)]
     >>> pref.keys()
     {'a_setting', 'a_number', 'a_bool'}
+    >>> hexchat.list_pluginpref()
+    [..., 'your_awesome_addon_name.a_setting',
+    'your_awesome_addon_name.a_number', 'your_awesome_addon_name.a_bool', ...]
     >>> for k in pref.keys(): del pref[k]
     >>> list(pref.items())
     []
+
     >>> del pref["doesn't exist"]
     KeyError: "doesn't exist"
     >>> pref.delete("doesn't exist")
@@ -61,16 +70,40 @@ class PluginPref(MutableMapping):
     NotImplemented
     """
 
-    def __init__(self, prefix):
-        self.prefix = prefix + "_"
+    def __init__(self, name=None, prefix_sep=".", prefix=None):
+        """Build a new mutable PluginPref mapping.
+
+        Arguments determine the prefix,
+        which will usually be constructed from the `__module_name__`,
+        given as the first parameter.
+
+        The module name will have surrounding spaces stripped,
+        spaces replaced by underscores,
+        be lowercased,
+        and have leading underscores stripped.
+
+        If desired, prefix may be specified
+        and will override unification of the passed module name.
+
+        prefix_sep is inserted between the prefix and key names (default '.').
+        """
+        if not (name or prefix):
+            raise TypeError("name or prefix must be provided")
+        self.prefix = prefix or name.strip().replace(" ", "_").lower().lstrip("_")
+        self.prefix_sep = prefix_sep
+        self.version_pref_name = "_version.%s" % self.prefix
+
+    def _keyname(self, key=""):
+        return self.prefix + self.prefix_sep + key
 
     def keys(self):
         """Return a set of all keys in this PluginPref instance."""
-        all_keys = hexchat.list_pluginpref(self.prefix)
+        shared_prefix = self._keyname()
+        all_keys = hexchat.list_pluginpref()
         keys = set()
         for key in all_keys:
-            if key.startswith(self.prefix):
-                keys.add(key[len(self.prefix):])
+            if key.startswith(shared_prefix):
+                keys.add(key[len(shared_prefix):])
         return keys
 
     def __iter__(self):
@@ -82,7 +115,7 @@ class PluginPref(MutableMapping):
     def __getitem__(self, key):
         if not isinstance(key, str):
             raise TypeError("Key must be a string")
-        val = hexchat.get_pluginpref(self.prefix + key)
+        val = hexchat.get_pluginpref(self._keyname(key))
         if val is None:
             raise KeyError(key)
         return val
@@ -92,7 +125,7 @@ class PluginPref(MutableMapping):
             raise TypeError("Key must be a string")
         if value is None:
             raise ValueError("Can not set `None`")
-        if not hexchat.set_pluginpref(self.prefix + key, value):
+        if not hexchat.set_pluginpref(self._keyname(key), value):
             raise RuntimeError("Could not set %s" % value)
 
     def __delitem__(self, key):
@@ -108,5 +141,5 @@ class PluginPref(MutableMapping):
         """
         if not isinstance(key, str):
             raise TypeError("Key must be a string")
-        if not hexchat.del_pluginpref(self.prefix + key):
+        if not hexchat.del_pluginpref(self._keyname(key)):
             raise RuntimeError("Could not delete %s" + key)
