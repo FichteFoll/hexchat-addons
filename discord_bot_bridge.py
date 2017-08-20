dc1f15c6b9dc1b1105961a7d0d7a0577916c804c
FichteFoll <fichtefoll2@googlemail.com>
2017-08-20 23:14:51 +0200

    Fix a shit ton of stuff for the bridge
    - The word list is not guaranteed to have 4 items.
    - Hook all relevant events instead of only some. (how did this happen?)
    - If colored nicks are enabled, the nick color is included in the word
      item, so we have to strip it before comparing.
    - The user list check was incorrect because it's a list of objects, not
      a list of strings.

--- discord_bot_bridge.py
+++ discord_bot_bridge.py
@@ -1,6 +1,6 @@
 __module_name__ = "Discord Bot Bridge"
 __module_author__ = "FichteFoll"
-__module_version__ = "0.1.1"
+__module_version__ = "0.2.0"
 __module_description__ = "Translates messages bridged from Discord into the native IRC protocol"
 
 import re
@@ -17,13 +17,38 @@ REVERSE_COLOR = "\026"
 ITALICS = "\035"
 
 
+def iter_fill(iterable, n, fillvalue=None):
+    """Iterate over iterable and append fillvalue until n values have been yielded."""
+    for x in iterable:
+        yield x
+        n -= 1
+    for _ in range(n):
+        yield fillvalue
+
+
+def is_user_in_channel(nick, context=hexchat):
+    for user in context.get_list('users'):
+        if hexchat.nickcmp(nick, user.nick) == 0:
+            return True
+    return False
+
+
 def msg_cb(word, word_eol, event_name, attrs):
-    nick, text, _, *other_word = word
+    # Trailing empty words are not included in the word list (here: mode_char, identified_text).
+    # For our custom print event, we don't care about the mode_char
+    # because we either override it mode char or don't do anything.
+    colored_nick, text, _, identified_text = iter_fill(word, 4, "")
+    nick = hexchat.strip(colored_nick)
+
     channel = hexchat.get_info('channel')
+    # if channel in BOT_MAP:
+    #     print("nick", repr(nick))
+    #     print("nickcmp", nick, BOT_MAP[channel], hexchat.nickcmp(nick, BOT_MAP[channel]))
+    #     print(".lower()==?", nick.lower() == BOT_MAP[channel].lower())
 
     if channel not in BOT_MAP:
         return hexchat.EAT_NONE
-    elif hexchat.nickcmp(nick, BOT_MAP[channel]):
+    elif hexchat.nickcmp(nick, BOT_MAP[channel]) != 0:
         return hexchat.EAT_NONE
 
     match = re.match(r"^<([^>]+)> (.*)", text)
@@ -34,11 +59,11 @@ def msg_cb(word, word_eol, event_name, attrs):
     if original_nick == hexchat.get_info('nick'):
         event_name = 'Your Message'
     else:
-        userlist = hexchat.get_list('users')
-        if original_nick not in userlist:
+        if not is_user_in_channel(original_nick):
             # TODO test if emit_print also works
             spaceless_nick = original_nick.replace(" ", "_")
-            hexchat.command("RECV {nick}!someone\@discord.server JOIN {channel}"
+            print("making", spaceless_nick, "join")
+            hexchat.command("RECV :{nick}!someone@discord.server JOIN {channel}"
                             .format(nick=spaceless_nick, channel=channel))
 
     # try to decect actions
@@ -49,13 +74,15 @@ def msg_cb(word, word_eol, event_name, attrs):
     # replace with proper escape code
     message = message.replace(REVERSE_COLOR, ITALICS)
 
-    hexchat.emit_print(event_name, original_nick, message, MODE_CHAR, *other_word, time=attrs.time)
+    hexchat.emit_print(event_name, original_nick, message, MODE_CHAR, identified_text,
+                       time=attrs.time)
 
     return hexchat.EAT_ALL
 
 
 if __name__ == '__main__':
-    for event in ('Channel Action', 'Channel Msg Hilight'):
+    for event in ('Channel Message', 'Channel Msg Hilight',
+                  'Channel Action', 'Channel Action Hilight'):
         hexchat.hook_print_attrs(event, msg_cb, event, hexchat.PRI_HIGHEST)
 
     print(__module_name__, __module_version__, "loaded")
